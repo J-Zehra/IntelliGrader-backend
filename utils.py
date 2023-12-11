54d5fd82a739281152471524f5bdaff4382b02ad
Jad <jazencode@gmail.com>
2023-12-12 07:25:20 +0800

    update

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def process(image, number_of_choices, correct_answer_indices):
 
     # DETECT CIRCLES
     circles = cv2.HoughCircles(
-        bubble_section_blur, cv2.HOUGH_GRADIENT, dp=1, minDist=10, param1=125, param2=10, minRadius=7, maxRadius=8
+        bubble_section_blur, cv2.HOUGH_GRADIENT, dp=1, minDist=10, param1=125, param2=10, minRadius=6, maxRadius=8
     )
 
     if circles is not None:
