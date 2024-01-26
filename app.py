1a5ee36626f616dacee99846b8e8732f57fd5e63
Jad <jazencode@gmail.com>
2024-01-26 10:31:19 +0800

    add progress

--- app.py
+++ app.py
@@ -12,32 +12,29 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+
 @socketio.on("grade")
 def handle_grade(data):
-    print("HERE")
     images = data["images"]
     answer = data["answer"]
     parts = data["parts"]
 
     response_data = []
     for index, image in enumerate(images):
-        print(index)
         image_array = np.frombuffer(image, dtype=np.uint8)
         image_original = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
         result = utils.process(image_original, parts, answer)
 
         if result["status"] == "error":
-            print("error")
             error_response = {
                 "message": result["message"],
                 "status": "failed",
                 "image": image,
             }
             response_data.append(error_response)
+            emit("progress", {"index": index + 1})
             continue
-
-        # socketio.emit("progress", {"index": index + 1})
 
         _, buffer = cv2.imencode(".webp", result["processed_image"])
         binary_image = buffer.tobytes()
@@ -54,6 +51,8 @@ def handle_grade(data):
         }
 
         response_data.append(data)
+        emit("progress", {"index": index + 1})
+        socketio.sleep(0.1)
 
     emit("grade_result", response_data)
 
