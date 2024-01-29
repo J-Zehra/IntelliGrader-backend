dccf16a376b71c4d00edc300401973279ab327f4
Jad <jazencode@gmail.com>
2024-01-29 08:41:28 +0800

    tenmp

--- app.py
+++ app.py
@@ -57,5 +57,23 @@ def handle_grade(data):
     emit("grade_result", response_data)
 
 
+@socketio.on("video")
+def handle_grade(data):
+    image = data["image"]
+    template_marker = cv2.imread("marker.jpg", 0)
+    template_marker_2 = cv2.imread("marker2.jpg", 0)
+
+    roll_number_section, roll_number_section_gray = utils.extract_section(image, template_marker_2, 5)
+    bubble_section, bubble_section_gray = utils.extract_section(image, template_marker, 10)
+
+    if roll_number_section is not None and bubble_section is not None:
+        response_data = {
+            "bubble_section": bubble_section,
+            "roll_number_section": roll_number_section
+        }
+
+        emit("detected", response_data)
+
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)
